a0f495ff0358f98d72131de2b0a48dd444905471
Damai Deo Saputro <damaideo6@gmail.com>
2023-10-13 08:25:32 +0700

    modif port

---
+++ pages/[slug].tsx
@@ -0,0 +1,229 @@
+import type { NextPage } from 'next'
+import React, { useEffect, useState } from 'react'
+import { Alert, Button, Card, CardBody, Input, Label } from '@roketid/windmill-react-ui';
+import API from '../app/API';
+import Loading from '../components/loading';
+import { useRouter } from 'next/router';
+import CryptoJS from "crypto-js";
+
+interface CustomerAbstract {
+  name: string,
+  position: string,
+  comments: string
+}
+
+interface AnswerAbstract {
+  id: number;
+  answer: number;
+}
+interface InstanceAbstract {
+  id: number,
+  name: string
+}
+
+
+interface QuestionAbstract {
+  id: number,
+  question: string
+}
+
+interface QuestionerAbstract {
+  value: number,
+  question: QuestionAbstract
+
+}
+
+interface isSubmitedAbstract {
+  success: boolean
+}
+// {
+//   "name": "test",
+//     "questioner": [
+//       {
+//         "value": 4,
+//         "question": {
+//           "id": 2
+//         }
+//       }
+//     ],
+//       "instance": "string"
+// }
+
+
+const Form = ({ name, position, onNameChange, onPositionChange }: any) => (
+  <form className='flex flex-col gap-4'>
+    <Label className='dark:text-gray-700'>
+      <span>Name</span>
+      <Input className="mt-1 dark:bg-white  dark:text-gray-800 dark:border-gray-400" onChange={(e) => onNameChange(e.target.value)} value={name} crossOrigin={undefined} />
+    </Label>
+    <Label className='dark:text-gray-700'>
+      <span>Position</span>
+      <Input className="mt-1 dark:bg-white dark:text-gray-800 dark:border-gray-400" onChange={(e) => onPositionChange(e.target.value)} value={position} crossOrigin={undefined} />
+    </Label>
+  </form>
+)
+
+
+const Home: NextPage = () => {
+  const router = useRouter()
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [customer, setCustomer] = useState<CustomerAbstract>({ name: "", position: "", comments: "" })
+  const [questioner, setQuestioner] = useState<QuestionerAbstract[]>([]);
+  const [question, setQuestion] = useState<QuestionAbstract[]>([])
+  const [indexQuestion, setIndexQuestion] = useState(-1);
+  const [isPrevious, setIsPrevious] = useState(indexQuestion != 0);
+  const [isNext, setIsNext] = useState(indexQuestion != question.length - 1 || indexQuestion == -1);
+  const [isSubmited, setIsSubmited] = useState<isSubmitedAbstract>()
+  const [instance, setInstance] = useState<InstanceAbstract>();
+  useEffect(() => {
+
+    if (!router.isReady) return;
+
+    setIsLoading(true)
+
+    API.get("question").then(res => {
+      setQuestion(res.data);
+      setIsLoading(false)
+    }).catch(e => console.log(e))
+
+
+    const chiperText = router.query.slug?.toString().replace(/p1L2u3S/g, '+').replace(/s1L2a3S4h/g, '/').replace(/e1Q2u3A4l/g, '=').toString() || "";
+    const bytes = CryptoJS.AES.decrypt(chiperText, process.env.KEY || "MERDEKA1945");
+    const data = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+    setInstance(data)
+
+  }, [router.isReady])
+
+  useEffect(() => {
+    checkNextPrevious();
+  }, [customer, indexQuestion, questioner])
+
+
+  const checkNextPrevious = () => {
+    if (indexQuestion == -1) {
+      if (!customer.name || !customer.position) {
+        setIsNext(false);
+      } else {
+        setIsNext(true);
+      }
+      return;
+    }
+    if (questioner[indexQuestion]?.value)
+      setIsNext(true)
+    else
+      setIsNext(false)
+    setIsPrevious(indexQuestion != -1)
+  }
+
+  const handleQuestion = (newQuestioner: QuestionerAbstract) => {
+    const data = [...questioner];
+    const index = data.findIndex((answer) => answer.question.id == newQuestioner.question.id);
+    if (index > -1) {
+      data[index] = newQuestioner;
+    } else {
+      data.push(newQuestioner);
+    }
+    setQuestioner(data);
+  }
+
+  const handleNext = () => {
+    if (!isNext) return;
+    if (indexQuestion == question.length - 1) {
+      submit();
+    }
+    else
+      setIndexQuestion(indexQuestion + 1);
+  }
+
+  const handlePrevious = () => {
+    if (!isPrevious) return;
+    setIndexQuestion(indexQuestion - 1);
+  }
+
+  const submit = () => {
+    setIsLoading(true)
+    API.post("/questioner", {
+      ...customer,
+      instance,
+      questioner
+    }).then(e => {
+      setIsLoading(false)
+      setIsSubmited({ success: true })
+    }).catch(e => {
+      setIsLoading(false)
+      setIsSubmited({ success: true })
+    })
+  }
+  const AnswerDesign = ({ data, index, isActive, onClick }: any) => {
+    return (
+      <div className="flex gap-2 items-center cursor-pointer "
+        onClick={onClick}>
+        <div
+          className={`rounded-full w-5 h-5 border-primary  ${isActive ? 'border-8' : 'border-4'}`}>
+
+        </div>
+        <p className='text-center text-sm'>{data}</p  >
+      </div >
+    )
+  }
+
+  const Question = () => {
+    return (
+      <>
+        <p>{question[indexQuestion].question} </p>
+        <div className="grid grid-cols-1 gap-y-4 mt-8 ">
+          {["Very Dissatisfied", "Dissatisfied", "Less Satisfied", "Satisfied", "Very Satisfied"].map((answerData, keyAnswer) => (
+            <AnswerDesign key={keyAnswer} data={answerData} isActive={questioner.find((answer) => answer.question.id === question[indexQuestion].id)?.value == keyAnswer + 1}
+              onClick={() => {
+                handleQuestion({ value: keyAnswer + 1, question: question[indexQuestion] })
+              }} />
+          ))}
+
+        </div>
+      </>
+    )
+  }
+
+
+  return (
+    <>
+      <div className='w-full min-h-screen flex justify-center items-center md:p-5 px-2 py-5'>
+        <Card className="mb-8 shadow-md dark:bg-white ring-gray-300 p-0  md:w-2/5 w-full  shadow-xl">
+          <CardBody className='bg-white p-10 md:px-10'>
+            <p className="text-2xl text-primary font-sans font-bold">iStow Survey</p>
+            <p className="text-1xl text-primary font-sans font-bold">{instance?.name}</p>
+
+            {!isSubmited ? (
+              <div className="mt-8">
+                {indexQuestion == -1 ? (
+                  < Form name={customer.name} position={customer.position} onNameChange={(name: string) => setCustomer({ ...customer, name })} onPositionChange={(position: string) => setCustomer({ ...customer, position })} />
+                ) : (
+                  <Question />
+                )}
+                <div className="flex">
+                  {indexQuestion != -1 && (
+                    <button onClick={handlePrevious} disabled={!isPrevious} className="disabled:bg-gray-200 disabled:hover:text-primary disabled:cursor-not-allowed  mt-6 text-sm bg-transparent hover:bg-primary text-primary hover:text-white py-2 px-4 border border-primary hover:border-transparent rounded">
+                      Previous
+                    </button>
+                  )}
+                  <button onClick={handleNext} disabled={!isNext} className="disabled:bg-gray-200 disabled:hover:text-primary disabled:cursor-not-allowed  disabled:border-transparent disabled:text-gray-400 disabled:hover:text-gray-400 self-end mt-6 ml-auto text-sm bg-transparent hover:bg-primary text-primary hover:text-white py-2 px-4 border border-primary hover:border-transparent rounded">
+                    {indexQuestion == question.length - 1 ? "Submit" : "Next"}
+                  </button>
+                </div>
+              </div>
+            ) : (<Alert className='dark:bg-green-50 dark:text-green-900 my-5' type={isSubmited.success ? 'success' : 'danger'} >Thank you for filling out the questionnaire</Alert>)}
+          </CardBody>
+          <div className="bg-primary w-full py-2 ">
+            {(indexQuestion > -1 && !isSubmited) && (<p className='text-center text-sm text-gray-200'> {questioner[indexQuestion]?.value ? indexQuestion + 1 : indexQuestion} of {question.length} answered </p>)}
+          </div>
+        </Card>
+      </div>
+
+      {isLoading && (<Loading />)}
+
+    </>
+  )
+}
+
+export default Home
---
+++ pages/admin/instance.tsx
@@ -0,0 +1,243 @@
+import React, { useState, useEffect, useRef, FormEvent } from 'react'
+
+import CryptoJS from "crypto-js";
+
+import PageTitle from '../../example/components/Typography/PageTitle'
+import SectionTitle from '../../example/components/Typography/SectionTitle'
+import {
+  Table,
+  TableHeader,
+  TableCell,
+  TableBody,
+  TableRow,
+  TableFooter,
+  TableContainer,
+  Badge,
+  Avatar,
+  Button,
+  Pagination,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Label,
+  Input,
+  Select,
+} from '@roketid/windmill-react-ui'
+import { EditIcon, MailIcon, PagesIcon, TrashIcon } from '../../icons'
+
+import Layout from '../../example/containers/Layout'
+import API from '../../app/API'
+import { useRouter } from 'next/router'
+interface ITableData {
+  id: number
+  name: string
+}
+
+
+const FormModal: React.FC<{ isOpen: boolean, onClose: Function, onSubmit: Function, idEdit: number }> = ({ isOpen, onClose, onSubmit, idEdit }: any) => {
+
+
+
+  const [name, setName] = useState("");
+
+
+
+  useEffect(() => {
+    if (idEdit == 0) return setName("");
+    API.get("/instance/" + idEdit).then(e => {
+      setName(e.data.name)
+    }).catch(e => {
+      console.log(e)
+    })
+
+  }, [idEdit])
+
+  const submit = (e: FormEvent) => {
+    e.preventDefault();
+    onSubmit(name)
+    onClose();
+    setName('')
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalHeader>{idEdit == 0 ? "Add Instance" : "Edit Instance"}</ModalHeader>
+      <form action="" onSubmit={submit}>
+        <ModalBody>
+          <Label className="mt-4">
+            <Input className="mt-1" placeholder="Instance name" crossOrigin={undefined} value={name} onChange={(e) => setName(e.target.value)} />
+          </Label>
+        </ModalBody>
+        <ModalFooter>
+          <div className="hidden sm:block">
+            <Button layout="outline" onClick={onClose}>
+              Cancel
+            </Button>
+          </div>
+          <div className="hidden sm:block">
+            <Button type='submit' onClick={submit}>Submit</Button>
+          </div>
+          <div className="block w-full sm:hidden">
+            <Button block size="large" layout="outline" onClick={onClose}>
+              Cancel
+            </Button>
+          </div>
+          <div className="block w-full sm:hidden">
+            <Button block size="large" type='submit'>
+              Submit
+            </Button>
+          </div>
+        </ModalFooter>
+      </form>
+    </Modal >
+  )
+}
+
+
+function Tables() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [idEdit, setIdEdit] = useState(0)
+  const [pageTable1, setPageTable1] = useState(1)
+  const [totalResults, setTotalResults] = useState(1)
+  const [dataTable1, setDataTable1] = useState<ITableData[]>([])
+  const [data, setData] = useState<ITableData[]>([])
+  const resultsPerPage = 10
+  const router = useRouter();
+  const [basePath, setBasePath] = useState("");
+
+
+  useEffect(() => {
+    const host = window.location.host;
+    const baseUrl = `http://${host}`;
+    setBasePath(baseUrl);
+    console.log(baseUrl)
+  }, [router.pathname]);
+
+
+  useEffect(() => {
+    console.log(router.basePath)
+    getInstance()
+  }, [])
+
+
+  useEffect(() => {
+    setPagination()
+  }, [data, dataTable1])
+
+
+  function onPageChangeTable1(p: number) {
+    setPageTable1(p)
+  }
+
+  const setPagination = () => {
+    setDataTable1(data.slice((pageTable1 - 1) * resultsPerPage, pageTable1 * resultsPerPage))
+  }
+
+  const getInstance = () => {
+    API.get("instance").then(res => {
+      setData(res.data);
+      setTotalResults(res.data.length);
+    }).catch(e => console.log(e))
+  }
+
+  const addInstance = (name: any) => {
+    API.post("/instance", {
+      name,
+    }).then(e => {
+      getInstance()
+    }).catch(e => {
+    })
+  }
+
+  const updateInstance = (name: any) => {
+    API.patch("/instance/" + idEdit, {
+      name,
+    }).then(e => {
+      getInstance()
+    }).catch(e => {
+    })
+  }
+
+  const removeQuestion = (id: number) => {
+    API.delete("/instance/" + id).then(e => {
+      getInstance()
+    }).catch(e => {
+    })
+  }
+
+
+  const handleFormSubmit = (e: any) => {
+    if (idEdit == 0)
+      addInstance(e)
+    else
+      updateInstance(e)
+  }
+  return (
+    <Layout>
+      <PageTitle>Instance</PageTitle>
+      <div className="flex gap-2 items-center">
+        <SectionTitle>Data Instance</SectionTitle>
+        <Button className="mb-4" size="small" onClick={() => {
+          setIdEdit(0)
+          setIsModalOpen(true)
+        }}>+</Button>
+      </div>
+      <TableContainer className="mb-8">
+        <Table>
+          <TableHeader>
+            <tr>
+              <TableCell>Name</TableCell>
+              <TableCell width={200}>Action</TableCell>
+            </tr>
+          </TableHeader>
+          <TableBody>
+            {dataTable1.map((data, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <span className="text-sm"> {data.name}</span>
+                </TableCell>
+                <TableCell>
+                  <div className="flex items-center space-x-4">
+                    <Button layout="link" size="small" aria-label="Edit" onClick={() => {
+                      setIdEdit(data.id)
+                      setIsModalOpen(true)
+                    }}>
+                      <EditIcon className="w-5 h-5" aria-hidden="true" />
+                    </Button>
+                    <Button layout="link" size="small" aria-label="Delete" onClick={() => removeQuestion(data.id)}>
+                      <TrashIcon className="w-5 h-5" aria-hidden="true" />
+                    </Button>
+                    <a href={basePath + "/" + CryptoJS.AES.encrypt(
+                      JSON.stringify(data),
+                      process.env.KEY || "MERDEKA1945"
+                    ).toString().replace(/\+/g, 'p1L2u3S').replace(/\//g, 's1L2a3S4h').replace(/=/g, 'e1Q2u3A4l')}>
+                      <Button layout="link" size="small" aria-label="Delete" >
+                        <MailIcon className="w-5 h-5" aria-hidden="true" />
+                      </Button>
+                    </a>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <TableFooter>
+          <Pagination
+            totalResults={totalResults}
+            resultsPerPage={resultsPerPage}
+            onChange={onPageChangeTable1}
+            label="Table navigation"
+          />
+        </TableFooter>
+      </TableContainer>
+      <FormModal idEdit={idEdit} isOpen={isModalOpen} onClose={() => {
+        setIsModalOpen(false)
+        setIdEdit(0);
+      }} onSubmit={handleFormSubmit} />
+    </Layout >
+  )
+}
+
+export default Tables

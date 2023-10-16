4eac27dd202b13c76ccae575a6f1999f6036907f
Damai Deo Saputro <damaideo6@gmail.com>
2023-10-16 09:26:52 +0700

    allow mix content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,21 @@ import 'tailwindcss/tailwind.css';
 import React from 'react'
 import { Windmill } from '@roketid/windmill-react-ui'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // suppress useLayoutEffect warnings when running outside a browser
   if (!process.browser) React.useLayoutEffect = React.useEffect;
 
   return (
-    <Windmill usePreferences={true} >
-      <Component {...pageProps} />
-    </Windmill>
+    <>
+      <Head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
+      <Windmill usePreferences={true} >
+        <Component {...pageProps} />
+      </Windmill>
+    </>
   )
 }
 export default MyApp
